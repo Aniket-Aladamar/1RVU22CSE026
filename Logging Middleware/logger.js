54f595dc7cca4e6c60f0e4a2e28fa8b56512c364
aniket <aniket.a@pixuate.com>
2025-09-08 10:59:44 +0530

    added logging middleware

---
+++ Logging Middleware/logger.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const LOG_API_URL = process.env.LOG_API_URL;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+const validStacks = ["backend", "frontend"];
+const validLevels = ["debug", "info", "warn", "error", "fatal"];
+const validPackages = [
+  "cache", "controller", "cron_job", "db", "domain", "handler", 
+  "repository","route","service"
+];
+
+async function Log(stack, level, pkg, message) {
+  try {
+    if (!validStacks.includes(stack)) throw new Error(`Invalid stack: ${stack}`);
+    if (!validLevels.includes(level)) throw new Error(`Invalid level: ${level}`);
+    if (!validPackages.includes(pkg)) throw new Error(`Invalid package: ${pkg}`);
+
+    const payload = { stack, level, package: pkg, message };
+
+    const response = await axios.post(LOG_API_URL, payload, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${ACCESS_TOKEN}`
+      }
+    });
+
+    console.log("Log sent:", response.data);
+    return response.data;
+
+  } catch (error) {
+    if (error.response) {
+      console.error("API Error:", error.response.data);
+    } else {
+      console.error("Failed to send log:", error.message);
+    }
+    return null;
+  }
+}
+
+// test cases
+(async () => {
+  await Log("backend", "error", "handler", "received string, expected bool");
+  await Log("backend", "fatal", "db", "Critical database connection failure.");
+})();
